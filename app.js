27c9589fbb0f5e81ac205da5046d9e51b1abdd96
Kiran R Patil <91089009+Kiran0R0Patil@users.noreply.github.com>
2022-04-30 21:49:10 +0530

    frog and basic log movement

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const squares = document.querySelectorAll('.grid div')
 
 let currentIndex = 76;
 // make a frog and track movement
-function moveFrog() {
+function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch(e.key){
+        case 'ArrowLeft':
+            if(currentIndex % 9 !== 0){
+            currentIndex --
+            }
+            break
+        case 'ArrowRight':
+            if((currentIndex+1) % 9 !== 0 ){
+            currentIndex++
+            }
+            break
+        case 'ArrowUp':
+            if(currentIndex > 8){
+            currentIndex -= 9
+            }
+            break
+        case 'ArrowDown':
+            if(currentIndex < 72){
+                currentIndex += 9
+            }
+            break
+    }
+    console.log(currentIndex)
     squares[currentIndex].classList.add('frog')
 }
 document.addEventListener('keyup', moveFrog)
-
-console.log(squares)
